1b5fd97c0cbf1d7dfaaecbfa07be1888054275fc
Delta <delta@Deltas-MacBook-Pro.local>
2023-07-05 17:06:47 +0530

    more consumers

--- subscriber.js
+++ subscriber.js
@@ -2,6 +2,8 @@ const amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', (err, connection) => {
   if (err) throw err;
+
+  // Consumer 1
   connection.createChannel((err, channel) => {
     if (err) throw err;
 
@@ -12,34 +14,58 @@ amqp.connect('amqp://localhost', (err, connection) => {
     channel.assertQueue('', { exclusive: true }, (err, q) => {
       if (err) throw err;
 
-      console.log('Waiting for messages...');
+      console.log('Consumer 1 is waiting for messages...');
 
       channel.bindQueue(q.queue, exchangeName, 'high');
-      channel.bindQueue(q.queue, exchangeName, 'medium');
-      channel.bindQueue(q.queue, exchangeName, 'lowl');
 
       channel.consume(q.queue, (msg) => {
-        console.log('Received:', msg.content.toString());
+        console.log('Consumer 1 received:', msg.content.toString());
+        channel.ack(msg);
+      });
+    });
+  });
+
+  // Consumer 2
+  connection.createChannel((err, channel) => {
+    if (err) throw err;
+
+    let exchangeName = 'directExchange';
+
+    channel.assertExchange(exchangeName, 'direct', { durable: false });
+
+    channel.assertQueue('', { exclusive: true }, (err, q) => {
+      if (err) throw err;
+
+      console.log('Consumer 2 is waiting for messages...');
+
+      channel.bindQueue(q.queue, exchangeName, 'medium');
+
+      channel.consume(q.queue, (msg) => {
+        console.log('Consumer 2 received:', msg.content.toString());
+        channel.ack(msg);
+      });
+    });
+  });
+
+  // Consumer 3
+  connection.createChannel((err, channel) => {
+    if (err) throw err;
+
+    let exchangeName = 'directExchange';
+
+    channel.assertExchange(exchangeName, 'direct', { durable: false });
+
+    channel.assertQueue('', { exclusive: true }, (err, q) => {
+      if (err) throw err;
+
+      console.log('Consumer 3 is waiting for messages...');
+
+      channel.bindQueue(q.queue, exchangeName, 'low');
+
+      channel.consume(q.queue, (msg) => {
+        console.log('Consumer 3 received:', msg.content.toString());
         channel.ack(msg);
       });
     });
   });
 });
-
-
-// amqp.connect(`amqp://localhost`, (err, connection) => {
-//     if(err) throw err;
-//     connection.createChannel((err, channel)=> {
-//         if(err) throw err
-
-//         let qname = "First Queue"
-//         let msg = "This is me sending my first message"
-//         channel.assertQueue(qname, {
-//             durable: false
-//         })
-//         channel.consume(qname, (msg) => {
-//             console.log(msg.content.toString())
-//             channel.ack(msg)
-//         })
-//     })
-// })
