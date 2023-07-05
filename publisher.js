5d578e708287935a8f3f83e9b00f88b7d427e710
Delta <delta@Deltas-MacBook-Pro.local>
2023-07-05 17:37:02 +0530

    normal message commented

--- publisher.js
+++ publisher.js
@@ -5,21 +5,9 @@ amqp.connect('amqp://localhost', (err, connection) => {
   connection.createChannel((err, channel) => {
     if (err) throw err;
 
-    let exchangeName = 'directExchange';
-    let msgHigh = 'High priority message';
-    let msgMedium = 'Medium priority message';
-    let msgLow = 'Low priority message';
-
-    channel.assertExchange(exchangeName, 'direct', { durable: false });
-
-    channel.publish(exchangeName, 'high', Buffer.from(msgHigh));
-    console.log('Published: ', msgHigh);
-
-    channel.publish(exchangeName, 'medium', Buffer.from(msgMedium));
-    console.log('Published: ', msgMedium);
-
-    channel.publish(exchangeName, 'low', Buffer.from(msgLow));
-    console.log('Published: ', msgLow);
+    let qName = "queue"
+    channel.assertQueue(qName, {durable : false});
+    channel.sendToQueue(qName, Buffer.from("Hello there this is me"))
 
     setTimeout(() => {
       connection.close();
@@ -27,3 +15,70 @@ amqp.connect('amqp://localhost', (err, connection) => {
     }, 1000);
   });
 });
+
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, connection) => {
+  if (err) throw err;
+
+  // Consumer 1
+  connection.createChannel((err, channel) => {
+    if (err) throw err;
+
+    let qName = "queue";
+    channel.consume(qName, (msg)=> {
+      
+        console.log(msg.content.toString());
+    })
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const amqp = require('amqplib/callback_api');
+
+// amqp.connect('amqp://localhost', (err, connection) => {
+//   if (err) throw err;
+//   connection.createChannel((err, channel) => {
+//     if (err) throw err;
+
+//     let qName = "queue"
+//     channel.assertQueue(qName, {durable : false});
+//     channel.sendToQueue(qName, Buffer.from("Hello there this is me"))
+
+//     setTimeout(() => {
+//       connection.close();
+//       process.exit(0);
+//     }, 1000);
+//   });
+// });
+
+
+// const amqp = require('amqplib/callback_api');
+
+// amqp.connect('amqp://localhost', (err, connection) => {
+//   if (err) throw err;
+
+//   // Consumer 1
+//   connection.createChannel((err, channel) => {
+//     if (err) throw err;
+
+//     let qName = "queue";
+//     channel.consume(qName, (msg)=> {
+      
+//         console.log(msg.content.toString());
+//     })
+//     });
+//   });
